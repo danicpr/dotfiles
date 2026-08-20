1a595242b9b3b2b8aaead6e6d916812ea8653257
Daniel Borre <danisecundarioxd@gmail.com>
2026-08-20 09:34:39 -0500

    Caelestia config

---
+++ dot_config/caelestia/hypr-vars.lua
@@ -0,0 +1,11 @@
+return {
+	terminal = "ghostty",
+	browser = "zen-browser",
+	editor = "zeditor",
+	cursorTheme = "MacTahoe-cursors",
+	cursorSize = 26,
+	touchpadScrollFactor = 0.6,
+
+	-- Binds
+	kbTerminal = { "SUPER + Return" },
+}
