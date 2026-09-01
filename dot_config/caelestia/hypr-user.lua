4ee24c7e59b962f26684611178e71abb097d0c81
Daniel Borre <danisecundarioxd@gmail.com>
2026-08-31 20:03:26 -0500

    a

--- dot_config/caelestia/hypr-user.lua
+++ dot_config/caelestia/hypr-user.lua
@@ -1,11 +1,12 @@
 hl.config({
-    input = {
-        kb_layout = "us",
-        kb_variant = "altgr-intl",
-    },
+	input = {
+		kb_layout = "us",
+		kb_variant = "altgr-intl",
+	},
 })
 
 hl.window_rule({
-    match = { class = "io.missioncenter.MissionCenter" },
-    workspace = "special:sysmon",
+	match = { class = "io.missioncenter.MissionCenter" },
+	workspace = "special:sysmon",
 })
+hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme MacTahoe-dark")
